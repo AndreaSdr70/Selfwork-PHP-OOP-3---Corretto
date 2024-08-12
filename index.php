9d9410ff9120c79dcd16a7c47778958d3716934e
AndreaSdr70 <andrea@sdringola.it>
2024-08-12 22:28:17 +0200

    Selfwork PHP OOP 3 - Corretto

---
+++ index.php
@@ -0,0 +1,120 @@
+<?php
+
+class Vertebrate
+{
+    public function __construct()
+    {
+$this->printVertebrate();
+    }
+    
+    protected function printVertebrate(){
+        echo "Sono un animale vertebrato \n";
+    }
+}
+
+class WarmBlood extends Vertebrate
+{
+
+    public function __construct()
+    {
+parent::__construct();
+    $this->printWarmBlood();
+    }
+    protected function printWarmBlood(){
+        echo "Sono un animale a sangue caldo \n";
+    }
+
+}
+
+class ColdBlood extends Vertebrate
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->printColdBlood();
+    }
+    protected function printColdBlood()
+    {
+        echo "Sono un animale a sangue freddo \n";
+    }
+}
+
+class Mammal extends WarmBlood 
+{
+
+    public function __construct()
+    {
+        parent::_construct();
+        $this->printMammal();        
+    }
+
+    protected function printMammal()
+    {
+        echo "Sono un mammifero \n";
+    }
+
+}
+
+class Bird extends WarmBlood
+{
+    public function __construct()
+    {
+        parent::__Construct();
+        $this->printBird();
+    }
+
+    protected function printBird()
+    {
+        echo "Sono un uccello \n";
+    }
+}
+
+class Fish extends ColdBlood
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->printFish();
+    }
+
+    protected function printFish()
+    {
+        echo "Sono un pesce \n";
+    }
+}
+
+class Reptile extends ColdBlood 
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->printReptile();
+    }
+
+    protected function printReptile()
+    {
+        echo "Sono un rettile \n";
+    }
+}
+
+class Amphibian extends ColdBlood 
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->printAmphibian();
+    }
+
+    protected function printAmphibian()
+    {
+        echo "Sono un anfibio \n";
+    }
+}
+$rana = new Amphibian();
+
+
+
+
+
+
+?>
